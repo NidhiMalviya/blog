0f8876f1f5c5b596380a9cc911296a354d2804d4
rails <mdubey@bestpeers.com>
2020-12-07 19:56:51 +0530

    first commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,58 @@
+class ArticlesController < ApplicationController
+    
+    http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
+    def index
+        @articles = Article.all
+    end
+
+    def show
+        @article = Article.find(params[:id])
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def create
+        @article = Article.new(article_params)
+ 
+        if @article.save
+            redirect_to @article
+        else
+            render 'new'
+        end
+    end
+
+    def update
+        @article = Article.find(params[:id])
+       
+        if @article.update(article_params)
+          redirect_to @article
+        else
+          render 'edit'
+        end
+    end
+
+
+    def destroy
+       @article = Article.find(params[:id])
+       @article.destroy
+   
+       respond_to do |format|
+        format.html { respond_to article_params, notice: 'Article was successfully remove '}
+        format.json { head :no_content }
+        format.js
+        end
+
+    end
+
+    private
+       def article_params
+       params.require(:article).permit(:title, :text)
+    end
+end
